5792ad2851e12938bc5c32fbda5ff788c6d4b202
nhoang169 <86874767+nhoang169@users.noreply.github.com>
2023-08-16 13:47:01 +0700

    Add files via upload

---
+++ app.js
@@ -0,0 +1,28 @@
+// Ex1
+// let a="Xin chao cac ban"
+// console.log(a);
+
+
+
+
+// Ex2
+
+// let input=prompt("Mời bạn nhập số tuổi của mình")
+
+
+// Ex3
+// document.write("Quyết tâm học lập trình javascript");
+
+// Ex4
+// let input= confirm("Chào mừng bạn đến với học viện Rikkei Academy")
+
+// Ex5
+// let userInput = prompt("Bạn đã đủ 18 tuổi chưa? (Nhập 'yes' hoặc 'no')");
+
+
+// Tinh dien tich hinh chu nhat
+
+let a = prompt("Nhập vào chiều dài")
+let b =prompt("Nhập vào chiều rộng")
+let sum=a*b;
+console.log(sum);
